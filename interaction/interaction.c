e701905a659067b23e9fd331a0bcd485069cee08
JPM <jpm@Mac-de-JPM.local>
2016-03-07 18:37:35 +0100

    zzz

---
+++ interaction/interaction.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curses.h>
+//Attention il faut compilet avec l'option -lncurses
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	char c, t[10];
+	int i;
+
+	//pour ncurses
+	initscr();
+
+        printw("--------\n");
+	printw("| MENU |\n");
+	printw("--------\n");
+	printw("\n\n");
+        printw("Choix A\n");
+        printw("Choix B\n");
+        printw("\n");
+        printw("Quitter Q\n");
+        printw("\n\n");
+	printw("Votre choix ? ");
+
+	// pour voir les printw à cause de ncurses
+	refresh();
+
+	c='\0';
+	while (c != 'Q')
+	{
+		c=getch();
+                move(14,0);
+                printw("                                                        ");
+                move(14,0);
+                // On efface les eventuelles trace de la ligne suivante
+
+		switch (c)
+		{
+			case 'A' :
+				printw("Vous avez choisi l'option A eeeeeeee\n");
+				break;
+                        case 'B' :
+				printw("Vous avez choisi l'option B\n");
+                                break;
+                        case 'Q' :
+                                break;
+                        default  :
+				printw("Cette option n'existe pas :(\n");
+				break;
+		}
+		// Repositionnement du curseur juste derriere "Votre choix ? "
+		// Le coin supérieur gauche à comme coordonnées 0,0 (ligne,colonne)
+		move(11,14);
+	}	
+
+	printw("\nA bientôt !\n");
+	refresh();
+	//delay de 2 secondes (exprimé en microsecondes) - sinon il y a simplement sleep
+	usleep(2000000);
+
+        //pour restaurer la console normale
+        endwin();
+	
+	return 0;
+}
---
+++ listechainee/origine/lst.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Element Element;
+struct Element
+{
+	int nombre;
+	Element *suivant;
+};
+
+typedef struct Liste Liste;
+struct Liste
+{
+	Element *premier;
+};
+
+Liste *initialisation()
+{
+	Liste *liste = malloc(sizeof(*liste));
+	Element *element = malloc(sizeof(*element));
+	
+	if (liste == NULL | element == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	element->nombre = 0;
+	element->suivant = NULL;
+	liste->premier = element;
+
+	return liste;
+}
+
+void insertion(Liste *liste, int nvNombre)
+{
+	Element *nouveau = malloc(sizeof(*nouveau));
+	if (liste == NULL || nouveau == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	nouveau->nombre = nvNombre;
+
+	nouveau->suivant = liste->premier;
+	liste->premier = nouveau;
+}
+
+void afficherListe(Liste *liste)
+{
+	if (liste == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	Element *actuel = liste->premier;
+
+	while (actuel != NULL)
+	{
+		printf("%d -> ", actuel->nombre);
+		actuel = actuel->suivant;
+	}
+	printf("NULL\n");
+}
+
+void suppression(Liste *liste)
+{
+	if (liste == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	if (liste->premier != NULL)
+	{
+		Element *aSupprimer = liste->premier;
+		liste->premier = liste->premier->suivant;
+		free(aSupprimer);
+	}
+}
+
+int main()
+{
+	Liste *maListe = initialisation();
+
+	insertion(maListe, 4);
+	insertion(maListe, 8);
+	insertion(maListe, 15);
+	suppression(maListe);
+
+	afficherListe(maListe);
+	return 0;
+}
